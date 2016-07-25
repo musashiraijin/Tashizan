3c723aa351646e5da9faddbb44288fc6bf58c6a1
Musashi Raijin <Raijin@raijin.local>
2016-07-25 18:40:01 +0900

    コンパイルに失敗する初期バージョン

--- Tashizan/ViewController.swift
+++ Tashizan/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tashizan1: UITextField!
+    @IBOutlet weak var tashizan2: UITextField!
+    @IBOutlet weak var result: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func resultButton(sender: UIButton) {
+        
+        var x1 = tashizan1.text.toInt()!
+        var x2 = tashizan2.text.toInt()!
+        
+        var y = x1 + x2
+        
+        result.text = String(y)
+        
+    }
+    
 }
 
